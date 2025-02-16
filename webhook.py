3753a46f756f933c452de24264cdd5412384edb9
EnzoFCh <enzofch@gmail.com>
2025-02-16 17:35:15 -0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET'])
 def verify():
@@ -15,6 +15,6 @@ def verify():
 def home():
     return "El servidor está funcionando correctamente."
 
-if name == 'main':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Render usa puertos dinámicos
     app.run(host="0.0.0.0", port=port)
