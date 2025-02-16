d863df605eb60070eb5b246978312a6501c24c81
EnzoFCh <enzofch@gmail.com>
2025-02-16 17:23:25 -0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 app = Flask(name)
@@ -6,9 +7,10 @@ app = Flask(name)
 def verify():
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
-    if token == "miwebhook123":  # Debe coincidir con lo que pongas en Meta
+    if token == "miwebhook123":
         return challenge
     return "Error de verificación", 403
 
 if name == 'main':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto dinámicamente
+    app.run(host="0.0.0.0", port=port)
